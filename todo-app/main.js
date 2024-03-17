89b6040e915738e3e9b2f56aaed5ba1db5f4294c
john ogheneochuko <ochustech56@gmail.com>
2024-03-17 12:38:20 +0100

    added a todo text input...

--- todo-app/main.js
+++ todo-app/main.js
@@ -1,3 +1,19 @@
 import './utils/bling'
 
-console.log($$('div'))
+//creating two function....
+
+//function one: App function
+function app(){
+//state
+//ui
+return mk('form',{id:'form'}, [
+    mk('input', {type:'text',id: 'todo', placeholder: 'Enter a Todo'}),
+    mk('button', {type: 'submit'}, ['Add Todo'])
+])
+}
+
+//function two: Rendering Function
+function render(){
+    document.body.append(app())
+}
+render()
