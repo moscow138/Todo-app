aff5d88816aa2d82d9d36aa1404324197d725ed0
john ogheneochuko <ochustech56@gmail.com>
2024-03-17 14:46:34 +0100

    added a new function.. added new style to css

--- todo-app/main.js
+++ todo-app/main.js
@@ -1,19 +1,44 @@
 import './utils/bling'
+import './style.css'
 
 //creating two function....
 
 //function one: App function
 function app(){
 //state
+
 //ui
-return mk('form',{id:'form'}, [
-    mk('input', {type:'text',id: 'todo', placeholder: 'Enter a Todo'}),
-    mk('button', {type: 'submit'}, ['Add Todo'])
-])
+let ui = {
+    input:$('input'),
+    form:$('form')
 }
+
+console.log(ui)
+
+return mk('div', {id:'app'}, [
+        
+        mk('h1',null, ['Todo App: John Ogheneochuko Version']),
+        (ui.form = mk('form',{id:'form'}, [
+        (ui.input = mk('input', {className:'todo', type:'text',id: 'todo', placeholder: 'Enter a Todo'})),
+        mk('button', {type: 'submit'}, ['Add Todo'])
+    ])),
+
+    mk('ul', {id:'todos'})
+]);
+//create a Todo Function..
+function createTodo(todo){
+    let item;
+
+    item = mk('li', {className: 'todo-item'}, [
+        mk('span', {},[todo.text])
+    ]);
+    return item;
+}
+createTodo({text: 'buy milk'})
+}// app() closing tag..
 
 //function two: Rendering Function
 function render(){
-    document.body.append(app())
+    document.body.prepend(app())
 }
 render()
