b8a3c0acd16b050f3a932a97dc76632606aa56d5
john ogheneochuko <ochustech56@gmail.com>
2024-04-07 16:19:03 +0100

    added deleteand update functions to the Todo App

--- todo-app/main.js
+++ todo-app/main.js
@@ -21,16 +21,58 @@ return mk('div', {id:'app'}, [
         (ui.form = mk('form',{id:'form'}, [
         (ui.input = mk('input', {className:'todo', type:'text',id: 'todo', placeholder: 'Enter a Todo'})),
         mk('button', {type: 'submit', onclick: add}, ['Add Todo'])
+        
     ])),
 
     (ui.todos = mk('ul', {id:'todos'}))
 ]);
 //create a Todo Function..
 function createTodo(todo){
-    let item, text;
+    let item, text, x;
+    // create Remove () => {}
+    const del = () => {
+        state.todos.filter((t) => t.id !== todo.id)
+        item.remove();
+
+    }
+    // Create An Edit function...
+    function edit(){
+        // event to submit and escape (Blur) out of the input..
+        function onKeyDown(event){
+            switch(event.key){
+                case 'Enter' : {
+                  text.textContent = todo.text = editInput.value;
+                  editInput.replaceWith(text);
+                    break;
+                }
+                case 'Escape' : {
+                    editInput.blur();
+                    break;
+                }
+               
+                default:
+                    break;
+            }
+
+        }
+        // cancel the Edit function..
+        const cancel = () => (x.disabled = editInput.replaceWith(text));
+
+        //create an edit input with mk() => {}
+       let editInput = mk('input', {style:'flex: 1;'}, { value: todo.text, onkeydown: onKeyDown, onblur: cancel })
+       // Replace the li with the input text..
+       text.replaceWith(editInput);
+       // Focus on The input..
+       editInput.focus();
+       // Making sure user can't delete during editing proccess...
+       x.disabled = true;
+       x.style = 'cursor: not-allowed;point-event: none'
+
+    }
 
     item = mk('li', {className: 'todo-item'}, [
-        (text = mk('span', {},[todo.text]))
+        (text = mk('span', { ondblclick: edit },[todo.text])),
+       (x = mk('button', {onclick: del}, ['x']))
     ]);
     return item;
 }
